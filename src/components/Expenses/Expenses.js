f463686ebdee69ef5a8ff71a9a8d948fa7b2a6e9
Meenal-Poladia <meenal.poladia94@gmail.com>
2021-07-05 19:55:25 +0530

    Added expense filter based on selected year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,16 +7,17 @@ import {useState} from "react";
 function Expenses(props) {
    const [selectedYear, setSelectedYear] = useState("2020");
 
+   const filteredList = props.items.filter(item => item.date.getFullYear() == selectedYear);
+
    const selectChangeHandler = (selectedYear) => {
       setSelectedYear(selectedYear);
-       console.log(selectedYear);
     }
 
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter selectedYear={selectedYear} onSelectedChange = {selectChangeHandler}/>
-                {props.items.map(expense => (
+                {filteredList.map(expense => (
                     <ExpenseItem key = {expense.id}
                                  title = {expense.title}
                                  amount = {expense.amount}
